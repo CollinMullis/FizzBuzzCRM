4e5fec8c4f153a5e4bba4b19cdd0b50ba550c00e
Collin Mullis <cmullis@saintjoe.edu>
2015-04-14 00:38:02 -0400

    Added "Buzz" Component

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -7,8 +7,12 @@
 // Print Buzz for #'s divisible by 5
 // Print FizzBuzz for #'s divisible by both 3 and 5
 for (i = 0; i <= 100;i++) {
+	// If # is divisible by 3, print "Fizz"
 	if (i % 3 == 0)
 		console.log("Fizz")
+	// If # is dibisible by 5, print "Buzz"
+	else if (i % 5 == 0)
+		console.log("Buzz")
 	else 
 		console.log(i)
 }

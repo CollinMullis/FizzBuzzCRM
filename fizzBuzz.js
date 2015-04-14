c9cccfd3446773f1a3c52911ae1bbd9ee80a9d44
Collin Mullis <cmullis@saintjoe.edu>
2015-04-14 00:34:14 -0400

    Added for loop to go from 1 to 100

---
+++ fizzBuzz.js
@@ -0,0 +1,11 @@
+/* Code by Collin Mullis
+	Javascript Exercise 
+	April 13th 2015
+*/
+// FizzBuzz, Print #'s 1 - 100
+// Print Fizz for #'s divisible by 3
+// Print Buzz for #'s divisible by 5
+// Print FizzBuzz for #'s divisible by both 3 and 5
+for (i = 0; i <= 100;i++) {
+	console.log(i)
+}

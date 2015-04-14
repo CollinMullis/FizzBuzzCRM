86768f2c11324eaa3529b01f7f0415924e74d4d3
Collin Mullis <cmullis@saintjoe.edu>
2015-04-14 00:41:32 -0400

    Added "FizzBuzz" Component and switched order of "If-else" statements

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -7,12 +7,15 @@
 // Print Buzz for #'s divisible by 5
 // Print FizzBuzz for #'s divisible by both 3 and 5
 for (i = 0; i <= 100;i++) {
-	// If # is divisible by 3, print "Fizz"
-	if (i % 3 == 0)
-		console.log("Fizz")
+	// If # is divisible by 3 and 5, print "FizzBuzz"
+	if (i % 3 == 0 && i % 5 == 0)
+		console.log("FizzBuzz")
 	// If # is dibisible by 5, print "Buzz"
 	else if (i % 5 == 0)
 		console.log("Buzz")
-	else 
+	// If # is divisible by 3, print "Fizz"
+	else if (i % 3 == 0)
+		console.log("Fizz")
+	else
 		console.log(i)
 }
